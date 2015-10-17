672454fdc73066670a4dbfde0db6e1c4f7ef2659
shllhs <shllhs@126.com>
2015-10-17 10:56:54 +0800

    1

---
+++ tracking/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main(int argc, char** argv)
+  {
+  mat A = randu<mat>(4,5);
+  mat B = randu<mat>(4,5);
+  
+  cout << A*B.t() << endl;
+  system("pause");
+  return 0;
+  }
